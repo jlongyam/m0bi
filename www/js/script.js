1885b8c9fd8399922e9b0e83e19cc342806fbf62
jlongyam <jhonliongbox@gmail.com>
2025-02-02 22:32:56 +0900

    Update

--- www/js/script.js
+++ www/js/script.js
@@ -1,8 +1,38 @@
 function fs_dir_list(path, cb) {
-    window.resolveLocalFileSystemURL(path, function(fileSystem) {
+    window.resolveLocalFileSystemURL(cordova.file[path], function(fileSystem) {
         var reader = fileSystem.createReader();
         reader.readEntries(function(entries) {
             cb(entries);
-        }, function(e) {});
-    }, function(e) {});
+        }, function(e) {cb(false)});
+    }, function(e) {cb(false)});
+}
+function fs_dir_create(path, name, cb) {
+    // PASS 'applicationStorageDirectory'
+    // PASS 'externalApplicationStorageDirectory'
+    window.resolveLocalFileSystemURL(cordova.file[path], function(dirEntry) {
+        dirEntry.getDirectory(name, {
+            create: true,
+            exclusive: true // fail if exists
+        }, function(entry) {
+            cb(entry.nativeURL);
+        }, function(e) {cb(false)}
+    )}, function(e) {cb(false)});
+}
+function fs_dir_rename(path, name_from, name_to) {
+    var old_path = path + name_from+'/';
+    return new Promise(function(resolve, reject) {
+        window.resolveLocalFileSystemURL(old_path, function(fs) {
+            fs.getParent(function(parent) {
+                fs.moveTo(parent,name_to, function(entry) {
+                    resolve(entry);
+                }, function(e) {
+                    reject(e)
+                }, function(e) {
+                    reject(e)
+                })
+            }, function(e) {
+                reject(e)
+            });
+        })
+    })
 }
